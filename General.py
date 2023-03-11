a99b14203fadc721e5b3f88c2e803b1230f77dbf
paulorogeriocp <paulorogeriocp@gmail.com>
2023-03-11 20:47:22 -0300

    Plot version with bar, line and histogram

--- General.py
+++ General.py
@@ -53,6 +53,7 @@ if uploaded_file:
      remove_nan_miss = st.checkbox('Remove NaN and missing rows?')   
      if remove_nan_miss: # Checkbox for NaN and missing values
        new_df=new_df.dropna() # Drop rows
+       new_df=new_df.reset_index(drop=True) # Reset index after drop the rows
        st.write("Amount of null and missing values by columns:", new_df.isna().sum())
 
 
@@ -61,11 +62,22 @@ if uploaded_file:
     #Plot a histogram of the first column
     st.header("4) Plot the data:")
     if columns_to_process: #In case there is at least a column selected
-        #val_count  = new_df[columns_to_process[0]].value_counts()
-        #fig = plt.figure(figsize=(10,5))
-        #sns.barplot(x=val_count.index, y=val_count.values, alpha=0.8)
-        #st.pyplot(fig)
-        df1 = new_df[columns_to_process[0]].value_counts()
-        st.bar_chart(df1)
+        plot_type = st.radio("Display a bar chart, line chart or histogram?", ("Bar chart", "Line chart", "Histogram"))
+        if plot_type=="Bar chart": # Plot a bar chart
+           column_to_plot = st.selectbox('Select the column for the plot:', columns_to_process) 
+           st.bar_chart(new_df[column_to_plot].value_counts())
+        elif plot_type=="Line chart": # Plot a line chart
+            x_to_plot = st.selectbox('Select the column for the x plot:', columns_to_process)
+            y_to_plot = st.selectbox('Select the column for the y plot:', columns_to_process)
+            st.line_chart(new_df, x=x_to_plot, y=y_to_plot)
+        else:
+            column_to_plot = st.selectbox('Select the column for the plot:', columns_to_process)
+            fig, ax= plt.subplots(figsize=(9, 3))
+            n_bins = st.slider('Select the column for the plot:', 1, int(new_df[column_to_plot].mean()/3), int(new_df[column_to_plot].mean()/9))
+            plot_kde = st.checkbox('Plot the Kernel Density Estimate (KDE)?')   
+            sns.histplot(data=new_df, x=column_to_plot, binwidth=n_bins, alpha=0.6, kde = plot_kde)
+            st.pyplot(fig)
+
+               
     
     
