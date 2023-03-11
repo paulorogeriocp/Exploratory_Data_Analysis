38de19ed8d64298dac744a6192cf4a1ab10a7583
paulorogeriocp <paulorogeriocp@gmail.com>
2023-03-11 10:18:49 -0300

    Four steps - raw, columns, cleaning and plot

--- General.py
+++ General.py
@@ -4,7 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.title("Exploratory Data Analysis on Streamlit")
+st.set_page_config(
+    page_title="Exploratory Data Analysis on Streamlit",
+    layout="wide"
+)
+
+st.title("Exloratory Data Analysis on Streamlit")
 
 # Load the data using a file imported by the user
 uploaded_file = st.file_uploader("Choose a CSV file:")
@@ -15,41 +20,52 @@ if uploaded_file:
     df = pd.read_csv(uploaded_file)
 
     
-    #First step - see raw data#
+    ########## First step - see raw data ##########
     # Show an amount of rows and columns
-    st.header("Show raw data:")
+    st.header("1) Show raw data:")
     n_rows, ncols=df.shape
     st.write("Your data has ", n_rows, " rows and ", ncols, " columns")
     amount_rows_show = st.slider("How many rows would you like to see?", 0, df.shape[0], 5)
-    columns_df = st.multiselect('Select which columns would you like to see:', options=df.columns, default=list(df.columns[0:5]))
+    columns_df = st.multiselect('Select which columns would you like to see:', options=df.columns, default=list(df.columns[0:11]))
     st.write("Raw data:")
     st.dataframe(df.head(amount_rows_show)[columns_df])
 
     #Descriptive statistics
-    st.write("Descriptive statistics: central tendency, dispersion and shape of a dataset’s distribution, excluding NaN values:")
+    st.write("Descriptive statistics: central tendency, dispersion and shape of a dataset’s distribution, excluding Not a Number (NaN) values:")
     st.dataframe(df.describe())
 
 
-    #Second step - data preparation#
+    ########## Second step - data preparation ##########
 
     #Drop unnecessary columns
+    st.header("2) Data preparation:")
     columns_to_process = st.multiselect('Select which columns would you like to keep. The others will be deleted', options=df.columns)
     new_df=df[columns_to_process].copy()
     st.dataframe(new_df)
+    
 
-    #To do
-    #Remove NaN and missing values
+    ########## Third step - remove NaN and missing values ##########
+
+    #Shows NaN and missing values by columns
+    st.header("3) Remove NaN and missing values:")
+    st.write("Amount of null and missing values by columns:", new_df.isna().sum())
+    if sum(new_df.isna().sum())>0: # Shows checkbox only if there is at least one NaN and missing value
+     remove_nan_miss = st.checkbox('Remove NaN and missing rows?')   
+     if remove_nan_miss: # Checkbox for NaN and missing values
+       new_df=new_df.dropna() # Drop rows
+       st.write("Amount of null and missing values by columns:", new_df.isna().sum())
 
 
-    #Third step - plot the data#
+    ########## Fourth step - plot the data ##########
 
     #Plot a histogram of the first column
+    st.header("4) Plot the data:")
     if columns_to_process: #In case there is at least a column selected
         #val_count  = new_df[columns_to_process[0]].value_counts()
         #fig = plt.figure(figsize=(10,5))
         #sns.barplot(x=val_count.index, y=val_count.values, alpha=0.8)
         #st.pyplot(fig)
-        df1 = new_df[columns_to_process[0]].value_counts().rename_axis('unique_values').reset_index(name='counts')
+        df1 = new_df[columns_to_process[0]].value_counts()
         st.bar_chart(df1)
     
     
